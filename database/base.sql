8f451a3c27b6b0bc6c17641d7dee92545528bea7
gboissel <gauthier.boissel@etu.univ-orleans.fr>
2026-03-02 18:10:13 +0100

    feat: initialisation première tables

---
+++ database/base.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Compagnie_Aerienne (
+     nomCompagnie VARCHAR2(50) PRIMARY KEY
+);
+CREATE TABLE Aeroport (
+    CodeIATA VARCHAR2(5) PRIMARY KEY,
+    Ville VARCHAR2(50),
+    CodePays VARCHAR2(2)
+
+);

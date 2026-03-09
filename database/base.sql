18f8d0571aa078edd5e08c742e45b844a0a803a7
gboissel <gauthier.boissel@etu.univ-orleans.fr>
2026-03-09 15:57:41 +0100

    preparation docker

--- database/base.sql
+++ database/base.sql
@@ -1,12 +1,20 @@
-CREATE TABLE Compagnie_Aerienne (
-     nomCompagnie VARCHAR2(50) PRIMARY KEY
-);
 CREATE TABLE Aeroport (
     CodeIATA VARCHAR2(5) PRIMARY KEY,
+    CodePays VARCHAR2(2),
     Ville VARCHAR2(50),
-    CodePays VARCHAR2(2)
-
+    nomAeroport VARCHAR2(50) UNIQUE
+    
 );
 CREATE TABLE Vol(
-    
+    nomCompagnie VARCHAR2(50),
+    numVol NUMBER,
+    dateHeureDep TIMESTAMP,
+    dateHeureArr TIMESTAMP,
+    terminalDep NUMBER,
+    terminalArr NUMBER,
+    depart VARCHAR2(5),
+    arriver VARCHAR2(5),
+    PRIMARY KEY (nomCompagnie,numVol,dateHeureDep)
+    FOREIGN KEY (depart) REFERENCES Aeroport(CodeIATA)
+    FOREIGN KEY (arriver) REFERENCES Aeroport(CodeIATA)
 )
